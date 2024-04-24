e933014624bbba5e0cbfecaf576b41d7171da8fd
josh <joshdfebbie@gmial.com>
2024-04-24 05:42:10 -0700

    middleware revision

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,24 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes:  ["/"]
-});
+export default authMiddleware({});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+
+
 
 // export const config = {
 //   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 // };
 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware()
+
+// export const config = {
+//   // The following matcher runs middleware on all routes
+//   // except static assets.
+//   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
