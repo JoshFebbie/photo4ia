e7a9e455e5360b607362f1ba987b86095b8209f5
josh <joshdfebbie@gmial.com>
2024-04-24 06:13:49 -0700

    second m-ware-revision

--- middleware.ts
+++ middleware.ts
@@ -9,16 +9,3 @@ export const config = {
 
 
 
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-// import { clerkMiddleware } from '@clerk/nextjs/server';
-
-// export default clerkMiddleware()
-
-// export const config = {
-//   // The following matcher runs middleware on all routes
-//   // except static assets.
-//   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
